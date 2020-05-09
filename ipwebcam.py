390755e9fa6f4275f12f594b2d67c2820556430a
Aravindhan C <64951915+aravindhanwow@users.noreply.github.com>
2020-05-09 17:25:05 +0530

    Delete ipwebcam.py

--- ipwebcam.py
+++
@@ -1,8 +0,0 @@
-import urllib
-import cv2
-import numpy as np
-
-url = "http://192.168.0.101:8080/shot.jpg?rnd=932497"
-imgResp = urllib.urlopen(url)
-imgRp = np.array(bytearray(imgResp.read()),dtype=np.unit8)
-
